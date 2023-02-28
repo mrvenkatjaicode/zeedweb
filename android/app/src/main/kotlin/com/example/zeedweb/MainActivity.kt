5634b4fc3e0ee7bed0d6d6371dee5a2455d5069e
venkatzeed <venkat@zeed.co.in>
2023-02-28 22:29:28 +0530

    initial commit
    initial commit

---
+++ android/app/src/main/kotlin/com/example/zeedweb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zeedweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'responsive.dart';
+import 'screens/desktop.dart';
+import 'screens/mobile.dart';
+import 'screens/tablet.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Responsive(mobile: Mobile(), tablet: Tablet(), desktop: Desktop()),
+    );
+  }
+}
